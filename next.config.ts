7ec5edcd406e24d5df9380dff1a1ef4ce1ea8d89
VictorYsla <victorysla98@gmail.com>
2025-04-14 15:00:47 -0500

    modify next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_HAWK_BASE_URL: process.env.NEXT_HAWK_BASE_URL,
+    NEXT_HAWK_INITIAL_PARAMS: process.env.NEXT_HAWK_INITIAL_PARAMS,
+    NEXT_HAWK_END_PARAMS: process.env.NEXT_HAWK_END_PARAMS,
+    NEXT_GOOGLE_MAPS_API_KEY: process.env.NEXT_GOOGLE_MAPS_API_KEY
+  },
 };
 
 export default nextConfig;
