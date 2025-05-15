057a64200970f70cdacf4002d6edd8b204620456
VictorYsla <victorysla98@gmail.com>
2025-05-15 12:10:43 -0500

    Add SVGS config

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
+      // issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
     return config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Logo from "@/assets/svgs/logo-01.svg";
+import Driver from "@/assets/svgs/icon-driver.svg";
+import Passenger from "@/assets/svgs/icon-passenger.svg";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-wari-gray flex flex-col items-center justify-center p-4 md:py-12">
       <div className="bg-white rounded-2xl px-4 py-8 w-full max-w-screen-md">
         <div className="flex flex-col items-center mb-8 md:mb-12">
-          <div className="w-24 h-24 md:w-32 md:h-32 my-10 relative">
-            <img src="/svgs/logo-01.svg" alt="Logo" />
+          <div className="w-24 h-24 md:w-32 md:h-32 mb-10 relative">
+            <Logo className="w-full h-full" />
           </div>
           <p className="font-montserrat-regular text-sm">Bienvenido a Wari,</p>
           <p className="font-montserrat-regular text-sm">
@@ -19,14 +23,10 @@ export default function Home() {
         </div>
 
         <div className="w-full flex flex-col md:flex-row justify-center items-stretch gap-6">
-          <div className="flex-1 border-none shadow-md bg-wari-yellow rounded-lg py-6 px-6">
+          <div className="flex-1 border-none shadow-md bg-wari-yellow rounded-3xl py-6 px-6">
             <div className="flex flex-col items-center pb-2">
               <div className="w-14 h-14 md:w-24 md:h-24 lg:w-32 lg:h-32 relative">
-                <img
-                  src="/svgs/icon-passenger.svg"
-                  alt="passenger"
-                  className="w-full h-full object-contain"
-                />
+                <Passenger className="w-full h-full" />
               </div>
               <h2 className="font-montserrat-bold text-[17px] ">Pasajero</h2>
             </div>
@@ -45,14 +45,10 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex-1 border-none shadow-md bg-wari-yellow rounded-lg py-6 px-6">
+          <div className="flex-1 border-none shadow-md bg-wari-yellow rounded-3xl py-6 px-6">
             <div className="flex flex-col items-center pb-2">
               <div className="w-14 h-14 md:w-24 md:h-24 lg:w-32 lg:h-32 relative">
-                <img
-                  src="/svgs/icon-driver.svg"
-                  alt="driver"
-                  className="w-full h-full object-contain"
-                />
+                <Driver className="w-full h-full" />
               </div>
               <h2 className="font-montserrat-bold text-[17px] ">Conductor</h2>
             </div>
