2b88b8e89dc0be45e42488296a86cafffc9a0c72
VictorYsla <victorysla98@gmail.com>
2025-05-17 12:29:48 -0500

    v3 for error screen

--- src/components/ErrorView.tsx
+++ src/components/ErrorView.tsx
@@ -1,13 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
-import { AlertCircle } from "lucide-react";
+import { CircleArrowLeft, CircleX, RotateCw } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 interface ErrorViewProps {
@@ -21,46 +12,41 @@ interface ErrorViewProps {
 export const ErrorView = ({ tripStatus, onGoBack }: ErrorViewProps) => {
   const router = useRouter();
   return (
-    <div className="min-h-screen bg-[#fffbeb] flex flex-col items-center justify-center px-4 py-8">
-      <div className="w-full max-w-md">
-        <Card className="border-amber-300 border-2 bg-[#fffbeb] p-6 shadow-sm">
-          <CardHeader className="px-0 pt-0">
-            <CardTitle className="text-2xl font-bold text-[#2a2416] mb-2">
+    <div className="min-h-screen bg-wari-gray flex flex-col items-center px-4 py-8 md:py-12">
+      <div className="bg-white rounded-3xl w-full max-w-screen-md  py-16 px-6 flex flex-col items-center">
+        <div className="border-wari-red border-2 px-6 pt-25 pb-6 shadow-sm w-full rounded-3xl flex flex-col justify-center items-center">
+          <div className="px-0 pt-0 w-full flex flex-col items-center text-center">
+            <CircleX className="h-20 w-20 stroke-[2.5] text-wari-red" />
+            <h2 className="font-montserrat font-bold text-xl text-wari-red mt-4">
               {tripStatus.message}
-            </CardTitle>
-            <p className="text-gray-700">
-              Ocurrió un problema al procesar tu solicitud
+            </h2>
+            <p className="font-montserrat font-bold text-sm my-10">
+              Ocurrió un problema al procesar
+              <br />
+              tu solicitud
             </p>
-          </CardHeader>
-          <CardContent className="space-y-6 px-0 pt-4">
-            <Alert className="bg-red-100 border-red-300 text-red-800">
-              <AlertCircle className="h-5 w-5 text-red-600 mr-2" />
-              <AlertTitle className="text-red-800 font-bold mb-1">
-                Error
-              </AlertTitle>
-              <AlertDescription className="text-red-700">
-                {tripStatus.description}
-              </AlertDescription>
-            </Alert>
+          </div>
 
-            <div className="space-y-3">
-              <Button
-                className="w-full bg-amber-300 hover:bg-amber-400 text-black py-6"
+          <div className="space-y-6 px-0 pt-4 w-full flex flex-col items-center">
+            <div className="space-y-3 w-full flex flex-col items-center">
+              <button
+                className="w-full md:w-80 bg-wari-black hover:bg-black-300 text-white text-[15px] font-montserrat font-bold py-3 px-8 rounded-4xl flex items-center justify-center gap-2 disabled:opacity-50"
                 onClick={onGoBack}
               >
+                <CircleArrowLeft className="h-6 w-6 stroke-[2.5] text-white" />
                 Volver al inicio
-              </Button>
+              </button>
 
-              <Button
-                variant="outline"
-                className="w-full border-gray-300 py-6 hover:bg-gray-100"
+              <button
+                className="w-full md:w-80 bg-wari-red hover:bg-red-400 text-white text-[15px] font-montserrat font-bold py-3 px-8 rounded-4xl flex items-center justify-center gap-2 disabled:opacity-50"
                 onClick={() => router.back()}
               >
+                <RotateCw className="h-6 w-6 stroke-[2.5] text-white" />
                 Intentar nuevamente
-              </Button>
+              </button>
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
