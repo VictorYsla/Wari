b1ab22e8bf67a08f66711c99bb43462dfd040a08
VictorYsla <victorysla98@gmail.com>
2025-05-17 01:48:01 -0500

    v3 for loading screen

--- src/components/LoadingView.tsx
+++ src/components/LoadingView.tsx
@@ -1,40 +1,38 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Luggage } from "lucide-react";
+import Destination from "@/assets/svgs/icon-dual-pin.svg";
+import Info from "@/assets/svgs/icon-info.svg";
 
 export const LoadingView = () => {
   return (
-    <div className="min-h-screen bg-[#fffbeb] flex flex-col items-center justify-center px-4 py-8">
-      <div className="w-full max-w-md flex flex-col items-center">
+    <div className="min-h-screen bg-wari-gray flex flex-col items-center px-4 py-8 md:py-12">
+      <div className="bg-white rounded-2xl px-2 w-full max-w-screen-md pt-25 pb-4 flex flex-col items-center">
         {/* Título de carga */}
-        <h1 className="text-3xl font-bold text-center mb-10 text-[#2a2416]">
+        <h1 className="font-montserrat font-bold text-xl text-center mb-20">
           Cargando
           <br />
-          información
-          <br />
-          del viaje...
+          información del viaje...
         </h1>
 
         {/* Animación de carga infinita */}
-        <div className="relative mb-10">
-          <div className="w-32 h-32 rounded-full border-8 border-amber-300/30"></div>
-          <div className="absolute top-0 left-0 w-32 h-32 rounded-full border-8 border-amber-300 border-t-transparent animate-spin"></div>
-          <div className="absolute top-0 left-0 w-32 h-32 flex items-center justify-center">
-            <Luggage className="w-12 h-12 text-black" />
+        <div className="w-full max-w-md mb-15 px-4 flex items-center justify-center ">
+          <div className="flex justify-center mt-4 flex-col w-50 items-center">
+            <Destination className="w-30 h-30 text-black mb-4" />
+            <div className="w-full h-5 bg-black rounded-full overflow-hidden p-1">
+              <div className="h-full bg-wari-yellow animate-progress rounded-full"></div>
+            </div>
           </div>
         </div>
 
         {/* Texto explicativo */}
-        <div className="text-center max-w-xs">
-          <p className="text-gray-700 mb-2">
-            Estamos obteniendo todos los detalles para&nbsp;ti.
+        <div className="rounded-3xl border-2 border-wari-yellow pt-10 pb-8 px-10 flex flex-col items-center w-full">
+          <Info className="w-6 h-6 mb-6" />
+          <p className="font-montserrat font-normal text-sm text-center leading-4">
+            Estamos obteniendo todos los
+            <br />
+            detalles para ti.
           </p>
-          <p className="text-gray-700">Esto puede tardar unos minutos.</p>
+          <p className="font-montserrat font-normal text-sm leading-4 mt-4">
+            Esto puede tardar unos minutos.
+          </p>
         </div>
       </div>
     </div>
