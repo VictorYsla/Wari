4e2b0b9eb59ca7545ce06d1727724a5b6e4a4513
VictorYsla <victorysla98@gmail.com>
2025-05-18 23:31:35 -0500

    add sitemap and robot

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://wari.hawkperu.com",
+  generateRobotsTxt: true, // (opcional) genera robots.txt automáticamente
+  sitemapSize: 7000,
+  // exclude: ['/secret-page'], // para excluir rutas si quieres
+};
